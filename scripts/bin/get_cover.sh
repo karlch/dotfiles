b9f95d11aca8212fc63ed349ba1b9674ffcf4222
karlch <christian.karl@protonmail.com>
2017-01-19 16:49:25 +0100

    Start cleaning up scripts

--- scripts/bin/get_cover.sh
+++ scripts/bin/get_cover.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 # Get the cover of an album in a folder
 
-file=$(ls | head -n 1)
-if !(echo ${file} | grep "ogg" 1>/dev/null 2>/dev/null); then
-    exit 1
-fi
+file=$(find . -type f -name "*.ogg" | head -n 1)
+[[ -z $file ]] && exit 0
 
-artist=$(exiftool ${file} | grep "Artist" | awk '{$1=""; $2=""; print $0}')
-album=$(exiftool ${file} | grep "Album" | awk '{$1=""; $2=""; print $0}')
+artist=$(exiftool "$file" | grep "Artist" | awk '{$1=""; $2=""; print $0}')
+album=$(exiftool "$file" | grep "Album" | awk '{$1=""; $2=""; print $0}')
 
-if (glyrc cover --artist "${artist}" --album "${album}" -w cover.jpg 2>/dev/null 1>/dev/null); then
-    printf "Fetched %s - %s\n" "${artist}" "${album}"
+if (glyrc cover --artist "$artist" --album "$album" -w cover.jpg 2>/dev/null 1>/dev/null); then
+    printf "Fetched %s - %s\n" "$artist" "$album"
 else
-    printf "Failed to fetch %s - %s\n" "${artist}" "${album}"
+    printf "Failed to fetch %s - %s\n" "$artist" "$album"
 fi
