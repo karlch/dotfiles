f43a6ebb6d58a8a0d607a55004a2564c9d0c6ea4
karlch <karlch@protonmail.com>
2018-11-05 16:09:09 +0100

    Update raspberry pi volume script
    * 5 percent steps
    * -t requires milliseconds
    * Slight text reformatting

--- i3/.i3/volnoti-set-raspberry.sh
+++ i3/.i3/volnoti-set-raspberry.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Update volume
-mpc volume ${1}2
+mpc volume ${1}5
 # Get current value
-vol=$(mpc volume | awk '{print $2}' | tr -d "%")
+vol=$(mpc volume | cut -d ":" -f2 | tr -d "%")
 # Display notification
-dunstify -i ~/Images/Inkscape/audio_icons/raspberry_small.png -r 43 -t 1 "Volume Pi" $vol" %"
+dunstify -i ~/Images/Inkscape/audio_icons/raspberry_small.png -r 43 -t 1500 "Volume Pi" $vol" %"
 # Print to info file for any other displaying purposes
 printf "%s\n" $vol > ~/.i3/info/volume_info_raspberry
